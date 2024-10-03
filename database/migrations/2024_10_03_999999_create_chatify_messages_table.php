dba9fae5776bf2da8f6d490d9c8690d715429ceb
Geraldo-p <gpaciencia68@gmail.com>
2024-10-03 14:02:43 +0100

    commit

--- database/migrations/2024_10_03_999999_create_chatify_messages_table.php
+++ database/migrations/2024_10_03_999999_create_chatify_messages_table.php
@@ -20,9 +20,11 @@ class CreateChatifyMessagesTable extends Migration
             $table->string('body',5000)->nullable();
             $table->string('attachment')->nullable();
             $table->boolean('seen')->default(false);
+            
             $table->timestamps();
         });
     }
+
 
     /**
      * Reverse the migrations.
